208f9fefdf5e32469780a28ce0e10f2a39ece6c4
Vinicius Rufine <vsrufine@gmail.com>
2018-09-12 17:32:42 -0300

    method da requisicao

--- api/index.js
+++ api/index.js
@@ -3,10 +3,15 @@ const url = require('url')
 
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true)
+
   const path = parsedUrl.pathname
   const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  const method = req.method.toLowerCase()
+
   res.end('hello world!\n')
-  console.log(`Request received on: ${trimmedPath}`)
+
+  console.log(`${method} > Request received on: ${trimmedPath}`)
 })
 
 server.listen(3000, () => {

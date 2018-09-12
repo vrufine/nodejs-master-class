46dab4cfb20b43f81bbc830a7621f8de0abc60c8
Vinicius Rufine <vsrufine@gmail.com>
2018-09-12 17:29:29 -0300

    parse na url requisitada

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 const http = require('http')
+const url = require('url')
 
 const server = http.createServer((req, res) => {
-  res.end('Server on!\n')
+  const parsedUrl = url.parse(req.url, true)
+  const path = parsedUrl.pathname
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+  res.end('hello world!\n')
+  console.log(`Request received on: ${trimmedPath}`)
 })
 
 server.listen(3000, () => {
